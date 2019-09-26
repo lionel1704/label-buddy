45d7728a104fb9b1bbae449aa6567492cc23968f
Lionel Faber <lionel1704@gmail.com>
2019-09-26 18:19:52 +0530

    use new approach using actions/toolkit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Label Buddy
+
+GitHub action to add or remove labels from an issue / pull request
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-const { Toolkit } = require('actions-toolkit')
+const core = require('@actions/core');
+const github = require('@actions/github');
 
-// Run your GitHub Action!
-Toolkit.run(async tools => {
-  const issue = tools.context.issue;
-  const args = tools.arguments;
-  console.log(args);
-  const addAction = await tools.github.issues.addLabels({
-    ...tools.context.repo,
-    issue_number: issue.number,
-    labels: ["testing"]
-  });
-  const removeAction = await tools.github.issues.removeLabel({
-    ...tools.context.repo,
-    issue_number: issue.number,
-    labels: ["testing"]
-  });
-  tools.exit.success('We did it!')
-}, { event: 'issues' });
+try {
+    const labelsToAdd = core.getInput('add');
+    console.log(`Labels to add: ${labelsToAdd}`);
+    const labelsToRemove = core.getInput('remove');
+    console.log(`Labels to remove: ${labelsToAdd}`);
+
+    const githubToken = core.getInput('githubToken');
+    const octokit = new github.GitHub(githubToken);
+    const context = github.context;
+    
+    octokit.issues.addLabels({
+        ...context.repo,
+        ...context.repo.owner,
+        ...context.payload.issue,
+        labels: labelsToAdd
+    });
+
+    labelsToRemove.forEach(label => {
+        octokit.issues.removeLabels({
+            ...context.repo,
+            ...context.repo.owner,
+            ...context.payload.issue.number,
+            label,
+        }); 
+    });
+} catch (error) {
+    core.setFailed(error.message)
+}
